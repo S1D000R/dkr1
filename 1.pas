3c1fb37f80e0215ed00f2eadf963ebc23cf53756
Maxim Isupov <124869886+S1D000R@users.noreply.github.com>
2023-12-11 11:07:02 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,10 @@
+﻿var x,y:real;
+begin
+  readln(x);
+  if x< -8 then y:=95*Power(x, 1/3) - x*x  else 
+    if x < 0 then y:= exp(x)-ln(x)/ln(x) else 
+      if x < 3 then y:= x*x*-x+tan(x)/ln(x) else 
+        y:= (ln(x)/log10(x))*(Power(x, 1/3)/cos(x)) ;
+  writeln(y);
+end.
+                                                                                   
---
+++ 2.pas
@@ -0,0 +1,13 @@
+﻿var x,y,a:real;
+begin
+  x := -10;
+  while x <= 5 do begin
+    if x< -8 then y:=95*Power(x, 1/3) - x*x  else 
+    if x < 0 then y:= exp(x)-ln(x)/ln(x) else 
+      if x < 3 then y:= x*x*-x+tan(x)/ln(x) else 
+        y:= (ln(x)/log10(x))*(Power(x, 1/3)/cos(x)) ;
+    x := x + 0.3;
+     writeln(y);
+  end;
+end.
+                                                                                   
